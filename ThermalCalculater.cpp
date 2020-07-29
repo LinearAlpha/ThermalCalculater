672251d208736c99482d6e7a20ac95b974077cd5
Minpyo Kim <45544800+LinearAlpha@users.noreply.github.com>
2020-07-29 11:04:32 -0500

    Initial commit

---
+++ ThermalCalculater.cpp
@@ -0,0 +1,14 @@
+﻿// ThermalCalculater.cpp : Defines the entry point for the application.
+//
+
+#include "ThermalCalculater.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	int x;
+	cin >> x;
+	return 0;
+}
